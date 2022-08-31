d88bfe739b683c89942fd4ff9c6085cea7d7b23a
yujinkim7123 <67150170+yujinkim7123@users.noreply.github.com>
2022-08-31 18:02:03 +0900

    Add files via upload

---
+++ crawling_puppeteer/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const cors = require("cors");
+const axios = require("axios")
+const cheerio = require("cheerio")
+app.use(cors());
+app.listen(PORT, () => {console.log(`서버가 ${PORT}로 켜졌다!`)})
+
+const url = "https://comic.naver.com/webtoon/weekdayList?week=mon"
+
+
+app.get("/scraper", async (req,res) => {
+  const response = await axios.get(url)
+  const html = response.data
+  const $ = cheerio.load(html)
+  const infos = []
+  $(".daily_img li").each((i, el) => {
+    const text = $(el).find("dt a").text()
+    const title = $(el).find("dt a").attr("title")
+    const url = $(el).find("dt a").attr("href")
+    infos.push({
+      text : text,
+      title: title,
+      url :  url
+    })
+  })
+  return res.json(infos)
+})
