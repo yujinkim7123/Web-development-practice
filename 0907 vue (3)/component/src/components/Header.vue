6d3edaaa755178cdbfc0b904e0c52d5436ec5d16
yujinkim7123 <67150170+yujinkim7123@users.noreply.github.com>
2022-09-07 16:56:25 +0900

    Add files via upload

---
+++ 0907 vue (3)/component/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    헤더
+  <Search></Search>
+  <Navigation></Navigation>
+  </div>
+</template>
+
+<script>
+import Search from './Search.vue'
+import Navigation from './Navigation.vue'
+
+export default {
+  components:{
+    Search,
+    Navigation
+  }
+
+}
+</script>
+
+<style>
+
+</style>
